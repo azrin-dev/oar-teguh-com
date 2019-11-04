d46e1aab0d6b266d523fd8d9776eb3502aea2225
oarteguh-com <me@azrin.dev>
2019-11-04 08:18:36 +0800

    Update Controllers

---
+++ start.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+// Make sure we are running node 12+
+const [major, minor] = process.versions.node.split('.').map(parseFloat);
+if (major < 11 || (major === 11 && minor <= 10)) {
+  console.log('Please run: sudo apt update node');
+  process.exit();
+}
+
+// import environmental variables from our variables.env file
+require('dotenv').config({ path: 'variables.env' });
+
+// Connect to our Database and handle any bad connections
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+mongoose.connection.on('error', (err) => {
+  console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+});
+
+// READY?! Let's go!
+// // Import db model
+require('./server/models/User');
+require('./server/models/Post');
+require('./server/models/Comment');
+require('./server/models/Like');
+
+const app = require('./app');
+app.set('port', process.env.PORT || 7777);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running → PORT ${server.address().port}`);
+});
